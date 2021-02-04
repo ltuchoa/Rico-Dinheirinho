d17beee56756a0ff4c52b976e253d4bf2ae00b2c
ltuchoa <ltuchoa@gmail.com>
2021-02-04 09:41:48 -0300

    created color extension and folders

---
+++ Rico Dinheirinho/Extensions/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Rico Dinheirinho
+//
+//  Created by Larissa Uchoa on 04/02/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryGreen = Color("primaryGreen")
+    static let secondaryGreen = Color("secondaryGreen")
+}
